5a34d2384217d7ca0450d7d17cf746e48bb6e0f7
Shubham Singh <shubhamsinghshubham777@gmail.com>
2025-09-03 10:28:22 +0530

    Migrate signing info from gradle.properties to build.gradle.kts & update release workflow gradle task

--- composebroadcasts/build.gradle.kts
+++ composebroadcasts/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
@@ -7,7 +9,7 @@ plugins {
 
 android {
     namespace = "compose.broadcasts"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         minSdk = 21
@@ -29,8 +31,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "17"
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_17)
+        }
     }
     buildFeatures {
         compose = true
@@ -40,4 +44,35 @@ android {
 dependencies {
     implementation(libs.androidx.ui)
     implementation(platform(libs.androidx.compose.bom))
+}
+
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
+    coordinates(groupId = "io.github.shubhamsinghshubham777", artifactId = "composebroadcasts")
+    pom {
+        name.set("Compose Broadcasts")
+        description.set("Compose Broadcasts provides the Jetpack Compose way of writing Broadcast Receiver code.")
+        inceptionYear.set("2024")
+        url.set("https://github.com/shubhamsinghshubham777/ComposeBroadcasts")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("shubhamsinghshubham777")
+                name.set("Shubham Singh")
+                url.set("https://github.com/shubhamsinghshubham777/")
+            }
+        }
+        scm {
+            url.set("https://github.com/shubhamsinghshubham777/ComposeBroadcasts")
+            connection.set("scm:git:git://github.com/shubhamsinghshubham777/ComposeBroadcasts.git")
+            developerConnection.set("scm:git:ssh://git@github.com/shubhamsinghshubham777/ComposeBroadcasts.git")
+        }
+    }
 }
--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -6,12 +8,12 @@ plugins {
 
 android {
     namespace = "compose.broadcasts.sample"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "compose.broadcasts.sample"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
@@ -31,8 +33,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "17"
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_17)
+        }
     }
     buildFeatures {
         compose = true
